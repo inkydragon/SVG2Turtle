aa7ec982f15940622d4170575f9a3834deb8b3e7
woclass <inkydragon@users.noreply.github.com>
2019-04-02 23:11:36 +0800

    help function v2

--- TurtleHelpFunc.py
+++ TurtleHelpFunc.py
@@ -1,40 +1,24 @@
 # -*- coding: utf-8 -*-
 import turtle as tur
+from math import sin, cos, pi
 
-"""
-全局变量定义
-    bezier_STEP 贝塞尔函数的取样次数
-    DRAW_SPEED  绘制速度
-    WIDTH       界面宽度
-    HEIGHT      界面高度
-"""
-bezier_STEP = 15
-T_RANGE = map(lambda x: x/bezier_STEP, range(0, bezier_STEP+1))
-T_LIST = [x/bezier_STEP for x in range(0, bezier_STEP+1)]
-DRAW_SPEED = 5
+DRAW_SPEED = 30
+PEN_SIZE = 6
 WIDTH = 600
 HEIGHT = 600
-
 
 """
 辅助函数
 """
-
-
-# 线性插值
 def linear_interp(p1, p2, t):
-    return p1*(1-t) + p2*t
+    return p1 * (1 - t) + p2 * t
 
-
-# 一阶 bezier 函数 - Tuple
 def bezier1(p1, p2, t):
     return (
         linear_interp(p1[0], p2[0], t),
         linear_interp(p1[1], p2[1], t)
     )
 
-
-# 二阶 bezier 函数
 def bezier2(p1, p2, p3, t):
     return bezier1(
         bezier1(p1, p2, t),
@@ -42,8 +26,6 @@ def bezier2(p1, p2, p3, t):
         t
     )
 
-
-# 三阶 bezier 函数
 def bezier3(p1, p2, p3, p4, t):
     return bezier1(
         bezier2(p1, p2, p3, t),
@@ -51,40 +33,26 @@ def bezier3(p1, p2, p3, p4, t):
         t
     )
 
-
 def bezier_3(p1, p2, p3, p4):
-    tur.pendown()
-    for t in T_LIST:
-        x,y = bezier3(p1, p2, p3, p4, t)
+    for t in [x / 15 for x in range(0, 15 + 1)]:
+        x, y = bezier3(p1, p2, p3, p4, t)
         tur.goto(x, y)
-    tur.penup()
 
 
 def p(c):  # 将 SVG 坐标 c 转换为 tur 坐标
     return complex(
-        + c.real - WIDTH/2,
-        - c.imag + HEIGHT/2
+        + c.real - WIDTH / 2,
+        - c.imag + HEIGHT / 2
     )
-
 
 def M(c):  # 移动到 SVG 坐标 c
     tur.goto(p(c).real, p(c).imag)
 
-
-"""
-绘图函数
-"""
-
-
-def Line(start, end):
-    # tur.penup()
+def L(start, end):
     M(start)
-    tur.pendown()
     M(end)
-    tur.penup()
 
-
-def CubicBezier(start, control1, control2, end):
+def C(start, control1, control2, end):
     M(start)
     bezier_3(
         (p(start).real, p(start).imag),
@@ -92,12 +60,57 @@ def CubicBezier(start, control1, control2, end):
         (p(control2).real, p(control2).imag),
         (p(end).real, p(end).imag))
 
+def f(sp=0j, rgb='red'):
+    tur.fillcolor(rgb)
+    M(sp)
+    tur.begin_fill()
 
-def Arc(a, *b): # 禁用
-    pass
+nf = tur.end_fill
 
+def pd(sp, rgb="#E4007F", bold=PEN_SIZE):
+    tur.color(rgb)
+    tur.pensize(bold)
+    M(sp)
+    tur.pendown()
 
+def pu():
+    tur.penup()
+    tur.pensize(PEN_SIZE)
+    tur.color("black")
 
+def E(
+        cx, cy,
+        a, b,
+        matrix=(1, 0, 0, 1, 0, 0)):
+    cp = complex(cx, cy)  # 中心
+    sp = cp + a  # 绘制起点
+    tur.penup()
+    M(transform(sp, matrix))
+    tur.color("#FB8D8C")
+    tur.pensize(0.1)
+    tur.fillcolor("#FB8D8C")
+    tur.begin_fill()
+    tur.pendown()
+    for i in range(0, 360 + 1):
+        x = a * sin(i * 2 * pi / 360 + pi / 2) + cx
+        y = b * cos(i * 2 * pi / 360 + pi / 2) + cy
+        p = complex(x, y)
+        M(transform(p, matrix))
+    nf()
+    pu()
+
+def transform(p, matrix=(1, 0, 0, 1, 0, 0)):
+    a, b, c = matrix[0], matrix[1], matrix[2],
+    d, e, f = matrix[3], matrix[4], matrix[5],
+    return complex(
+        a * p.real + c * p.imag + e,
+        b * p.real + d * p.imag + f
+    )
+
+def Mv(c):  # 移动到 SVG 坐标 c
+    tur.penup()
+    tur.goto(p(c).real, p(c).imag)
+    tur.pendown()
 
 
 """
@@ -106,8 +119,8 @@ def Arc(a, *b): # 禁用
 screen = tur.Screen()
 screen.bgcolor("white")
 
-tur.tracer(100)   # 调整作画时间
-tur.pensize(1)
+tur.tracer(3)  # 调整作画时间
+tur.pensize(PEN_SIZE)
 tur.speed(DRAW_SPEED)
 tur.penup()
 
@@ -115,4 +128,3 @@ tur.penup()
 
 tur.hideturtle()  # 隐藏乌龟图标
 tur.update()
-
